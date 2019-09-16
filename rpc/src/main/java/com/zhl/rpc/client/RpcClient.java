da307e2debe25b7477bf67c10a1fb37cb68b4d76
zhuhailong <13121141179@163.com>
2019-09-16 09:06:23 +0800

    feat(rpc client): add rpc client Add RpcClient to Send RpcRequest and receive RpcResponse Andd RpcProxy：Generate a proxy object for each class and send the rpc request in the invoke function

---
+++ rpc/src/main/java/com/zhl/rpc/client/RpcClient.java
@@ -0,0 +1,82 @@
+package com.zhl.rpc.client;
+
+import com.zhl.rpc.common.RpcEncoder;
+import com.zhl.rpc.common.RpcRequest;
+import com.zhl.rpc.common.RpcResponse;
+import com.zhl.rpc.register.RpcDiscover;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @program codeX
+ * @description: RPC客户端，通过网络通信往RPC服务端发送请求调用信息，接受服务端的响应消息，配置动态代理类
+ * @author: meilong
+ * @create: 2019/09/16 08:14
+ */
+public class RpcClient extends SimpleChannelInboundHandler<RpcResponse> {
+    private RpcResponse rpcResponse;
+
+    private RpcRequest rpcRequest;
+
+    private Object object = new Object();
+
+    private RpcDiscover rpcDiscover;
+
+    public RpcClient(RpcRequest rpcRequest, RpcDiscover rpcDiscover) {
+        this.rpcRequest = rpcRequest;
+        this.rpcDiscover = rpcDiscover;
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, RpcResponse msg) throws Exception {
+        this.rpcResponse = msg;
+        // 同步锁,资源对象
+        synchronized (object) {
+            ctx.flush();
+            object.notifyAll();
+        }
+    }
+
+    public RpcResponse send() throws Exception {
+        Bootstrap client = new Bootstrap();
+        NioEventLoopGroup loopGroup = new NioEventLoopGroup();
+        client.group(loopGroup)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline()
+                                .addLast(new RpcEncoder(RpcRequest.class))
+                                .addLast(new RpcEncoder(RpcResponse.class))
+                                // .....
+                                .addLast(RpcClient.this);
+
+                    }
+                }).option(ChannelOption.SO_KEEPALIVE, true);
+
+        // 获取一个服务器地址
+        String serverAddress = rpcDiscover.discover();
+        String host = serverAddress.split(":")[0];
+        int port = Integer.valueOf(serverAddress.split(":")[1]);
+        ChannelFuture future = client.connect(host, port).sync();
+
+        System.out.println("客户端准备发送数据:" + rpcRequest);
+        future.channel().writeAndFlush(rpcRequest).sync();
+
+        synchronized (object) {
+            object.wait();
+        }
+        if (rpcResponse != null) {
+            future.channel().closeFuture().sync();
+        }
+        return rpcResponse;
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
---
+++ rpc/src/main/java/com/zhl/rpc/client/RpcProxy.java
@@ -0,0 +1,48 @@
+package com.zhl.rpc.client;
+
+import com.zhl.rpc.common.RpcRequest;
+import com.zhl.rpc.common.RpcResponse;
+import com.zhl.rpc.register.RpcDiscover;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.UUID;
+
+/**
+ * @program codeX
+ * @description:动态代理类，用于获取到每个类的代理对象
+ * @author: meilong
+ * @create: 2019/09/16 08:51
+ */
+public class RpcProxy {
+    private RpcDiscover rpcDiscover;
+
+    /**
+     * RpcProxy对于每一个类都创建一个动态代理对象，并且在invoke方法创建rpc客户端并且发送网络通信请求
+     *
+     * @param interfaceClass
+     * @param <T>
+     * @return
+     */
+    public <T> T getInstance(Class<T> interfaceClass) {
+        T instance = (T) Proxy.newProxyInstance(interfaceClass.getClassLoader(), new Class<?>[]{interfaceClass}, new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                RpcRequest rpcRequest = new RpcRequest();
+                String className = method.getDeclaringClass().getName();
+                Class<?>[] parameterTypes = method.getParameterTypes();
+
+                rpcRequest.setRequestId(UUID.randomUUID().toString());
+                rpcRequest.setClassName(className);
+                rpcRequest.setParameterTypes(parameterTypes);
+                rpcRequest.setParameters(args);
+                rpcRequest.setMethodName(method.getName());
+                RpcResponse rpcResponse = new RpcClient(rpcRequest, rpcDiscover).send();
+                return rpcResponse.getResult();
+            }
+        });
+        // 返回一个代理对象
+        return instance;
+    }
+}
